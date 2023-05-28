e504499e76b836b65a23757902fb754fc8b8760c
gessfred <gessfred@protonmail.com>
2023-05-28 21:29:06 +0200

    improve prompt

--- main.py
+++ main.py
@@ -26,34 +26,32 @@ def complete_chat(chat):
 
 #openai.organization = "YOUR_ORG_ID"
 def main():
-    config = dotenv_values(".env")
-    openai.api_key = config["OPENAI_API_KEY"]
-
-    status = get_command('git status')
 
     diff = get_command("git diff")
     log = get_command("git log --oneline")
     codebase = get_command("cat main.py")
     prompt=[
         {"role": "user", "content": """
-            You are a bot that generates git commit messages that are succinct and descriptive, only based on git output.
-            You should not be exhaustive, and only describe what seems to matter in the diff. 
-            Try to guess what the change is primarily doing.
-            For example if the diff contains a configuration change but also a new feature, focus on the feature.
-            However if there is only a configuration change, then you can mention it in the commit message.
-            Also don't just say what is changed but try to understand why.
-            Don't mention the file names in the commit message.
-            Try to keep the headline short. You can generate a multiline commit message if (and only if) needed.
+            You are a bot that generates git commit messages that are succinct and descriptive, only based on git diff.
+            You should not be exhaustive, and only describe what seems to matter in the diff.
+            Your message should be context-aware but only focus on what was added or removed.
+            The message should be about what the change is primarily doing.
+            For example, if the diff contains a config change and a new feature, focus on the feature.
+            However if there is only a config change, then you can mention it in the commit message.
+            Don't just say what is changed but explain intent.
+            Keep the headline short.
         """},
         {"role": "user", "content": f"""
-            First, here is the full content of file(s) that were updated {codebase}
             Here is the output of git diff: ```{diff}```
-            Here is the current git log: {log}
-            Come up with a helpful commit message and don't mention the file name"""}
+            Come up with a helpful commit message and don't mention the file names in the commit message.
+            """
+        }
     ]
     commit_message = complete_chat(prompt)
     print(commit_message)
     log_event({"diff": diff, "message": commit_message})
 
 if __name__ == '__main__':
+    config = dotenv_values(".env")
+    openai.api_key = config["OPENAI_API_KEY"]
     main()
